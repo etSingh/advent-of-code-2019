3e200418278604ce940a845c7b45618f848ab4a3
Harpreet Singh <singhh@tcd.ie>
2019-12-08 21:41:12 +0000

    Add solution for day 5

--- src/main/scala/example/Day5.scala
+++ src/main/scala/example/Day5.scala
@@ -10,10 +10,13 @@ object Day5 {
   val IMMEDIATE_MODE = 1
   val POSITION_MODE = 0
 
-  case class Values(v1: Int, v2: Int, v3: Int)
+  trait Parameters
 
-  case class Operation(opCode: Int, values: Option[Values])
+  case class ThreeParams(v1: Int, v2: Int, v3: Int) extends Parameters
+  case class TwoParams(v1: Int, v2: Int) extends Parameters
+  case class OneParam(v1: Int) extends Parameters
 
+  case class Operation(opCode: Int, values: Option[Parameters])
   object HaltOperation extends Operation(opCode = 99, values = None)
 
   object Operation {
@@ -27,15 +30,19 @@ object Day5 {
       val mode1 = ins / 100 % 10
       val mode2 = ins / 1000 % 10
 
-      if (opCode == 1 || opCode == 2) {
-        Operation(opCode, Some(Values(
+      if (List(1, 2, 7, 8) contains opCode) {
+        Operation(opCode, Some(ThreeParams(
           getValue(mode1, intCode, index + 1),
           getValue(mode2, intCode, index + 2),
           intCode(index + 3))))
-      } else {
+      } else if (List(5, 6) contains opCode){
+        Operation(opCode, Some(TwoParams(
+          getValue(mode1, intCode, index + 1),
+          getValue(mode2, intCode, index + 2))))
+      }
+      else {
         val value = getValue(mode1, intCode, index + 1)
-        Operation(opCode, Some(
-          Values(value, value, value))) // FIXME
+        Operation(opCode, Some(OneParam(value)))
       }
     }
 
@@ -47,20 +54,64 @@ object Day5 {
         intCode(position)
       }
     }
+
+    def performLogicalOp(updatedIntCode: ArraySeq[Int], params: ThreeParams)
+                        (logicalOp: (Int, Int) => Boolean): ArraySeq[Int] = {
+      if (logicalOp(params.v1, params.v2))
+        updatedIntCode.updated(params.v3, 1)
+      else
+        updatedIntCode.updated(params.v3, 0)
+    }
+
+    def getInstructionPointer(params: TwoParams, index: Int, updatedIntCode: ArraySeq[Int])
+                             (test: Int => Boolean) =
+      if (test(params.v1)) params.v2
+      else index + 3
+
   }
 
   def performComputation(intCode: ArraySeq[Int], code: Int, index: Int): (Int, ArraySeq[Int]) =
     Operation.parseOperation(code, intCode, index) match {
-      case HaltOperation => (0, intCode)
-      case Operation(opCode, Some(values)) =>
+      case HaltOperation => (index + 0, intCode)
+      case Operation(opCode, Some(params: ThreeParams)) =>
         opCode match {
-          case 1 => (4, intCode.updated(values.v3, values.v1 + values.v2))
-          case 2 => (4, intCode.updated(values.v3, values.v1 * values.v2))
-          case 4 =>
-            println(values.v1)
-            (2, intCode)
+          case 1 => (index + 4, intCode.updated(params.v3, params.v1 + params.v2))
+          case 2 => (index + 4, intCode.updated(params.v3, params.v1 * params.v2))
+          case 7 => (index + 4, Operation.performLogicalOp(intCode, params)((x, y) => x < y))
+          case 8 => (index + 4, Operation.performLogicalOp(intCode, params)((x, y) => x == y))
+        }
+      case Operation(4, Some(param: OneParam)) =>
+        output = param.v1
+        println(param.v1)
+        (index + 2, intCode)
+      case Operation(opCode, Some(params: TwoParams)) =>
+        opCode match {
+          case 5 => (Operation.getInstructionPointer(params, index, intCode)(x => x != 0), intCode)
+          case 6 => (Operation.getInstructionPointer(params, index, intCode)(x => x == 0), intCode)
         }
     }
+
+  def getInstructionPointer(index: Int, updatedIntCode: ArraySeq[Int], test: Int => Boolean) = {
+    val firstParameter = updatedIntCode(index + 1)
+    if (test(updatedIntCode(firstParameter))) {
+      val secondParameter = updatedIntCode(index + 2)
+      Some(updatedIntCode(secondParameter))
+    } else {
+      None
+    }
+  }
+
+  def performLogicalOp(updatedIntCode: ArraySeq[Int], index: Int, logicalOp: (Int, Int) => Boolean): ArraySeq[Int] = {
+    val firstParameter = updatedIntCode(index + 1)
+    val secondParameter = updatedIntCode(index + 2)
+    val thirdParameter = updatedIntCode(index + 3)
+    if (logicalOp(updatedIntCode(firstParameter), updatedIntCode(secondParameter)))
+      updatedIntCode.updated(thirdParameter, 1)
+    else
+      updatedIntCode.updated(thirdParameter, 0)
+  }
+
+  var output = Integer.MAX_VALUE
 
   def runIntCode(intCode: ArraySeq[Int], input: Int): ArraySeq[Int] = {
     @tailrec
@@ -79,13 +130,27 @@ object Day5 {
           compute(index + 2, updatedIntCode.updated(position, input))
         case 4 =>
           val value = updatedIntCode(index + 1)
+          output = updatedIntCode(value)
           println(updatedIntCode(value))
           compute(index + 2, updatedIntCode)
+        case 5 =>
+          getInstructionPointer(index, updatedIntCode, x => x != 0) match {
+            case Some(pointer) => compute(pointer, updatedIntCode)
+            case None => compute(index + 3, updatedIntCode)
+          }
+        case 6 => getInstructionPointer(index, updatedIntCode, x => x == 0) match {
+          case Some(pointer) => compute(pointer, updatedIntCode)
+          case None => compute(index + 3, updatedIntCode)
+        }
+        case 7 =>
+          compute(index + 4, performLogicalOp(updatedIntCode, index, (x: Int, y: Int) => x < y))
+        case 8 =>
+          compute(index + 4, performLogicalOp(updatedIntCode, index, (x: Int, y: Int) => x == y))
         case 99 =>
           updatedIntCode
         case opCode =>
-          val (i, update) = performComputation(updatedIntCode, opCode, index)
-          compute(index + i, update)
+          val (newIndex, update) = performComputation(updatedIntCode, opCode, index)
+          compute(newIndex, update)
       }
     }
 
@@ -97,9 +162,16 @@ object Day5 {
       intCode <- extractIntCode(5)
     } yield runIntCode(ArraySeq.from(intCode), 1).toList.head
 
+  def part2(): Either[String, Int] =
+    for {
+      intCode <- extractIntCode(5)
+    } yield runIntCode(ArraySeq.from(intCode), 5).toList.head
 
-  def main(args: Array[String]): Unit = {
-    print(part1())
+
+  def getOutput(intCode: ArraySeq[Int], input: Int): Int = {
+    val res = runIntCode(intCode, input)
+    output // Dirty hack
   }
+
 
 }
--- src/test/scala/example/Week1Spec.scala
+++ src/test/scala/example/Week1Spec.scala
@@ -1,7 +1,9 @@
 package example
 
-import example.Day4.{compute, matchesAtMostCriteria, matchesAtLeastCriteria}
+import example.Day4.{compute, matchesAtLeastCriteria, matchesAtMostCriteria}
 import org.scalatest._
+
+import scala.collection.immutable.ArraySeq
 
 class Week1Spec extends FlatSpec with Matchers {
 
@@ -29,4 +31,42 @@ class Week1Spec extends FlatSpec with Matchers {
     compute(136818, 685979, matchesAtMostCriteria) shouldEqual 1291
     compute(136818, 685979, matchesAtLeastCriteria) shouldEqual 1919
   }
+
+  "IntCode computer for day 5 part 2" should "work" in {
+    val eqToEight = ArraySeq.from(List(3,9,8,9,10,9,4,9,99,-1,8))
+    val eqToEightImmediateMode = ArraySeq.from(List(3,3,1108,-1,8,3,4,3,99))
+
+    Day5.getOutput(eqToEight, 8) shouldEqual 1
+    Day5.getOutput(eqToEight, 10) shouldEqual 0
+    Day5.getOutput(eqToEightImmediateMode, 8) shouldEqual 1
+    Day5.getOutput(eqToEightImmediateMode, 10) shouldEqual 0
+
+    val lessThanEight = ArraySeq.from(List(3,9,7,9,10,9,4,9,99,-1,8))
+    val lessThanEightImmediateMode = ArraySeq.from(List(3,3,1107,-1,8,3,4,3,99))
+
+    Day5.getOutput(lessThanEight, 7) shouldEqual 1
+    Day5.getOutput(lessThanEight, 10) shouldEqual 0
+    Day5.getOutput(lessThanEightImmediateMode, 7) shouldEqual 1
+    Day5.getOutput(lessThanEightImmediateMode, 10) shouldEqual 0
+
+    val jumpNonZero = ArraySeq.from(List(3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9))
+    val jumpNonZeroImmediateMode = ArraySeq.from(List(3,3,1105,-1,9,1101,0,0,12,4,12,99,1))
+
+    Day5.getOutput(jumpNonZero, 7) shouldEqual 1
+    Day5.getOutput(jumpNonZero, -7) shouldEqual 1
+    Day5.getOutput(jumpNonZero, 0) shouldEqual 0
+    Day5.getOutput(jumpNonZeroImmediateMode, 7) shouldEqual 1
+    Day5.getOutput(jumpNonZeroImmediateMode, -7) shouldEqual 1
+    Day5.getOutput(jumpNonZeroImmediateMode, 0) shouldEqual 0
+
+    val largeExample = ArraySeq.from(List(3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+      1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+      999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99))
+
+    Day5.getOutput(largeExample, 7) shouldEqual 999
+    Day5.getOutput(largeExample, 8) shouldEqual 1000
+    Day5.getOutput(largeExample, 9) shouldEqual 1001
+
+  }
+
 }
